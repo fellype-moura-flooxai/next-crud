83499c2d55272404357ea363e5940987203a08bb
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-04-24 11:40:35 -0300

    feat: add dependencies

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className={`
+    flex justify-center items-center h-screen
+    bg-gradient-to-r from-blue-500 to-purple-500 text-right`}>
+      <h1>App</h1>
+    </div>
+  )
+}
