dc37417ce52a10893b786afa7cae04bffb1aba47
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-04-24 13:39:17 -0300

    feat: Button Component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Layout from "./components/Layout";
 import Tabela from "./components/Tabela";
 import Cliente from "../core/Cliente";
+import Botao from "./components/Botao";
 
 export default function Home() {
 
@@ -24,6 +25,9 @@ export default function Home() {
     flex justify-center items-center h-screen
     bg-gradient-to-r from-blue-500 to-purple-500 text-white`}>
       <Layout titulo="Cadastro Simples">
+        <div className="flex justify-end">
+        <Botao cor="green" className="mb-4">Novo Cliente</Botao>
+        </div>
         <Tabela clientes={clientes} 
         clienteSelecionado={clienteSelecionado}
         clienteExcluido={clienteExcluido}
