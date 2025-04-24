dd73fbe3fbbe89c0eb71ca3ed4c0cca4d3b88f2c
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-04-24 11:58:41 -0300

    feat: layout and title components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
+import Layout from "./components/Layout";
+
 export default function Home() {
   return (
     <div className={`
     flex justify-center items-center h-screen
-    bg-gradient-to-r from-blue-500 to-purple-500 text-right`}>
-      <h1>App</h1>
+    bg-gradient-to-r from-blue-500 to-purple-500 text-white`}>
+      <Layout titulo="Cadastro Simples">
+        <span>Conteudo</span>
+      </Layout>
     </div>
   )
 }
